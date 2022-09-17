9480141a769eadf47ae9a74d5320061cc574f624
YUJUNGCHEN <a0922335447@gmail.com>
2022-09-17 21:05:34 +0800

    feat: login view ui setup

--- src/main.js
+++ src/main.js
@@ -7,9 +7,13 @@ import router from "./router";
 import "bootstrap";
 import "./assets/all.scss";
 
+import Vue3Lottie from "vue3-lottie";
+import "vue3-lottie/dist/style.css";
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(Vue3Lottie);
 
 app.mount("#app");
